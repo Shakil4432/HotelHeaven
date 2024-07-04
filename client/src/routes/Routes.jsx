e0efd0e7cd922b86b0915790d41f44338e0c9a83
shakil4432 <shakil4432@gmail.com>
2024-07-04 15:25:43 +0600

    first commit

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,27 +1,49 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Main from '../layouts/Main'
-import Home from '../pages/Home/Home'
-import ErrorPage from '../pages/ErrorPage'
-import Login from '../pages/Login/Login'
-import SignUp from '../pages/SignUp/SignUp'
-import RoomDetails from '../pages/RoomDetails/RoomDetails'
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import RoomDetails from "../pages/RoomDetails/RoomDetails";
+import DashboardLayout from "../layouts/DashboardLayout";
+import Statistics from "../pages/Dashboard/Common/Statistics";
+import AddRoom from "../pages/Dashboard/Host/AddRoom";
+import MyListings from "../pages/Dashboard/Host/MyListings";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/room/:id',
+        path: "/room/:id",
         element: <RoomDetails />,
       },
     ],
   },
-  { path: '/login', element: <Login /> },
-  { path: '/signup', element: <SignUp /> },
-])
+  {
+    path: "/dashboard",
+    element: <DashboardLayout></DashboardLayout>,
+    children: [
+      {
+        index: true,
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "add-room",
+        element: <AddRoom></AddRoom>,
+      },
+      {
+        path: "my-listings",
+        element: <MyListings />,
+      },
+    ],
+  },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <SignUp /> },
+]);
